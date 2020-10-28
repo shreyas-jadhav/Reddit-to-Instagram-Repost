aa4163e2fc04addae8b30120cb2208d141a2020f
shreyasjadhav4 <73429007+shreyasjadhav4@users.noreply.github.com>
2020-10-27 23:23:18 -0700

    Update igUpload.py

--- igUpload.py
+++ igUpload.py
@@ -15,7 +15,7 @@ class ig:
         self.bot.upload_story_photo(photo=photo, upload_id=None)
 
         # Customize your caption here----------------------------------------
-        content = str(title) + "\n\nPosted by: " + un + "\n\nFollow: @"+str(self.username) + " for more #davie504memes #davie504 #bassmemes #bassist #bassistmemes"
+        content = str(title) + "\n\nPosted by: " + un + "\n\nFollow: @"+str(self.username) + " for more "
         # -------------------------------------------------------------------
         try:
 
